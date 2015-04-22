f9a1711a9ac439a698d432498824ad22801ba327
Mark <mark@bearcodi.com>
2015-04-22 16:16:31 +1000

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@
         $('#agency').on('click', 'a', function(e){
             e.preventDefault();
             var $el = $(this);
+            var $elGet = $('#get-extra').empty();
             var searchKey = $el.data('search');
             var searchURI = $el.data('uri');
             
@@ -16,7 +17,7 @@
             
             if ($el.data('get')) {
                 var searchGet = $el.data('get').split('&');
-                var $elGet = $('#get-extra').empty();
+                
                 $.each(searchGet, function(i, d) {
                     var parts = d.split('=');
                     $('<input type="hidden" />')
