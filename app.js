7f697c5bd30c4e0c8dcdec054895fe65cd0e4b76
Mark <mark@bearcodi.com>
2015-04-22 15:56:13 +1000

    Update app.js

--- app.js
+++ app.js
@@ -3,5 +3,10 @@
         $('#lucky').on('click', function (e) {
             $('#search').val('punk');
         });
+        
+        $('#agency').on('click', 'a', function(e){
+            e.preventDefault();
+            console.log(this);
+        });
   });
 }(jQuery));
